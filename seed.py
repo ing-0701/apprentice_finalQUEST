1459bbedcb645d8834b62d4a88911aedfd6da393
ing-0701 <haruina3716@gmail.com>
2026-03-27 12:48:32 +0900

    stage3とepilogueのDB追加

--- seed.py
+++ seed.py
@@ -26,6 +26,39 @@ def run():
         )
         
         
+    Texts.objects.filter(stage_tag='stage3').delete()
+    
+    stage3_texts = [
+        "じゃが、最後に確かめたいことがある。",
+        "お前は本当に……心の底から、あの子を愛しているのか？",
+        "ならば、証明してみせよ！ 喉が枯れ、指が動かなくなるまで、その想いを叫び続けるのじゃ！",
+        "さあ、お前の真実を……その叫びを、私に刻みつけてみせろ！！",
+    ]
+    
+    for i, text in enumerate(stage3_texts, start=1):
+        Texts.objects.create(
+            stage_tag='stage3',
+            order=i,
+            content=text
+        )
+    
+    
+    Texts.objects.filter(stage_tag='epilogue').delete()
+    
+    epilogue_texts = [
+        "王に認められた僕たちは、正式に結婚した",
+        "王座に就かないと約束した通り、僕は一般貴族として暮らすことになった",
+        "料理の勉強をして、毎日姫のために食事を作っている　最近は体調もずいぶんよくなった",
+        "そうして僕たちは、いつまでも幸せに暮らした",
+    ]
+    
+    for i, text in enumerate(epilogue_texts, start=1):
+        Texts.objects.create(
+            stage_tag='epilogue',
+            order=i,
+            content=text
+        )
+        
         
     GatekeeperFlag.objects.all().delete()
     GatekeeperFlag.objects.create(
